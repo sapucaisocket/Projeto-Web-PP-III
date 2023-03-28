0aea53910db2158495545c0a2cd4d75407dacbcd
Válcler Manoel Vieira Camelo Maia <valcler.manoel.vieira06@aluno.ifce.edu.br>
2023-03-27 23:20:02 -0300

    login_estilizado

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express");
+var path = require('path');
+global.authTokens = {};
+var loginRouter = require('./routes/login');
 const app = express();
 const admin = require("./routes/admin")
+const login = require("./routes/login")
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Rotas:
 app.use('/', admin)
+app.use('/login', login)
 
 //Porta do local host do projeto
 app.listen(8081, function() {
