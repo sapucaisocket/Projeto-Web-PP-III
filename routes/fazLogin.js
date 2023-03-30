48a35012d9f03d89d22e28670eff47803647c5e9
Nicolas Marcelino <nicolas.marcelino.mota08@aluno.ifce.edu.br>
2023-03-30 00:13:10 -0300

    Atualização do gitignore

--- routes/fazLogin.js
+++ routes/fazLogin.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://login-study-975a8-default-rtdb.firebaseio.com"
+});
+
 /* POST login. */
 router.post('/', function(req, res, next) {
     const { usuario, senha } = req.body;
